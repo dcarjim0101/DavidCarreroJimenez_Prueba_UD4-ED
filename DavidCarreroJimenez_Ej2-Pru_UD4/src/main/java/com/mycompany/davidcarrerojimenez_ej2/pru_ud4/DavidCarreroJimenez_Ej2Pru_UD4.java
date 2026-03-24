e913938e37f427452c125ef9bbd47f8145491650
dcarjim0101 <dcarjim0101@g.educaand.es>
2026-03-24 09:42:17 +0100

    Add files via upload

---
+++ DavidCarreroJimenez_Ej2-Pru_UD4/src/main/java/com/mycompany/davidcarrerojimenez_ej2/pru_ud4/DavidCarreroJimenez_Ej2Pru_UD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.davidcarrerojimenez_ej2.pru_ud4;
+
+/**
+ *
+ * @author 02_1DAW_alum
+ */
+public class DavidCarreroJimenez_Ej2Pru_UD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
